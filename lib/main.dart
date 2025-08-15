a95cc65118358fa5f67c614ad15569c049dcd295
istiaq66 <ahmedboby66@gmail.com>
2025-08-15 21:38:23 +0600

    Integrate ThemeProvider and SharedPreferences
    This commit introduces `ThemeProvider` to manage app themes and integrates `SharedPreferences` for theme persistence.
    
    Key changes:
    - Added `provider` and `shared_preferences` dependencies.
    - Modified `main.dart` to initialize `SharedPreferences` and provide `ThemeProvider` using `MultiProvider`.
    - The `MyApp` widget now accepts `SharedPreferences` and wraps the `MaterialApp` with `MultiProvider` to make `ThemeProvider` available throughout the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
+import 'package:auction_app/providers/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:auction_app/Screens/auth_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp( const MyApp());
+  final prefs = await SharedPreferences.getInstance();
+  runApp(MyApp(sharedPreferences: prefs));
 }
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final SharedPreferences sharedPreferences;
+  const MyApp({super.key, required this.sharedPreferences});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ThemeProvider>(create: (_) => ThemeProvider(sharedPreferences)),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AuthPage(),
+      ),
     );
   }
 }
